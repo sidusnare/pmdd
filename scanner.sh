f72958bf0f1d945f9bd2136c9547b7a69c725620
Fred Dinkler IV <bugs@solrpime.net>
2013-05-08 13:19:54 -0400

    scanner.sh notes

--- scanner.sh
+++ scanner.sh
@@ -11,9 +11,11 @@ for x in iconv grep sed find sort;do
 done
 
 if [ -d "$1" ];then
+	#Find files, get their md5sum, ignore the md5sum of zero length files, strip invalid UTF-8 (the linker dosent like it)
 	find "$1" -xdev -type f -exec md5sum '{}' \; | grep -v d41d8cd98f00b204e9800998ecf8427e | iconv -c -t UTF-8 > file_list.txt
 else
 	echo "You need to specify a directory"
 	exit
 fi
+#Sort it
 sort -o sorted_file_list.txt file_list.txt
